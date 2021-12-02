51880c90d6644819530519afc46f13f3a2df09c8
luke <lf.abbott@gmail.com>
2021-12-02 12:25:06 -0800

    Standard Syntax and package.json

--- Recruiter Aid/src/FileCabinet/SuiteScripts/fc_repodemo2_recruiter_aid.js
+++ Recruiter Aid/src/FileCabinet/SuiteScripts/fc_repodemo2_recruiter_aid.js
@@ -3,19 +3,18 @@
  * @NScriptType Suitelet
  */
 define([],
-    
-    () => {
-        /**
+
+  () => {
+    /**
          * Defines the Suitelet script trigger point.
          * @param {Object} scriptContext
          * @param {ServerRequest} scriptContext.request - Incoming request
          * @param {ServerResponse} scriptContext.response - Suitelet response
          * @since 2015.2
          */
-        const onRequest = (scriptContext) => {
+    const onRequest = (scriptContext) => {
 
-        }
+    };
 
-        return {onRequest}
-
-    });
+    return { onRequest };
+  });
